79e597906c4ab3dcb3e26938353770be855c170a
Kingdez04 <desmondademeso@gmail.com>
2025-08-19 00:31:44 +0100

    feat: add bottom tabs navigation for native app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,62 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 const _layout = () => {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarLabelStyle: { fontSize: 13.65, fontWeight: "semibold" },
+        tabBarStyle: {
+          height: 60,
+        },
+        tabBarLabelPosition: "below-icon",
+      }}
+    >
       <Tabs.Screen
         name="index"
-        options={{ title: "Home", headerShown: false }}
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={24}
+              color={focused ? "#6A9BCC" : "gray"}
+            />
+          ),
+        }}
       />
+
       <Tabs.Screen
         name="history"
-        options={{ title: "History", headerShown: false }}
+        options={{
+          title: "History",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "time" : "time-outline"}
+              size={24}
+              color={focused ? "#6A9BCC" : "gray"}
+            />
+          ),
+        }}
       />
+
       <Tabs.Screen
         name="settings"
-        options={{ title: "Settings", headerShown: false }}
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              size={24}
+              color={focused ? "#6A9BCC" : "gray"}
+            />
+          ),
+        }}
       />
     </Tabs>
   );
 };
+
 export default _layout;
